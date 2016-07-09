9529b4f8a90730155e9d84254fb5969db32447e4
Dmitry <dsheiko@intrigma.com>
2016-07-09 16:06:12 +0200

    Create a login screen layout

---
+++ app/src/main/java/ca/itquality/patrol/LoginActivity.java
@@ -0,0 +1,38 @@
+package ca.itquality.patrol;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initStatusBar();
+    }
+
+    /**
+     * Makes the status bar transparent.
+     */
+    private void initStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+    }
+
+    /**
+     * Required for the font library.
+     */
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+}
---
+++ app/src/main/java/ca/itquality/patrol/app/MyApplication.java
@@ -0,0 +1,17 @@
+package ca.itquality.patrol.app;
+
+import android.app.Application;
+
+import ca.itquality.patrol.R;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Roboto-RobotoRegular.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+    }
+}
